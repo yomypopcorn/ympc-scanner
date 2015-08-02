dbdeb1aa01b895eb3438af5b475b642d9d41368a
Max Kueng <me@maxkueng.com>
2015-08-02 22:52:49 +0200

    fix config

--- dbclient.js
+++ dbclient.js
@@ -2,7 +2,7 @@ var db = require('ympc-dbclient');
 var config = require('./config');
 
 exports = module.exports = db({
-  socket: config['redis-docket'],
+  socket: config['redis-socket'],
   host: config['redis-host'],
   port: config['redis-port'],
   password: config['redis-password']
